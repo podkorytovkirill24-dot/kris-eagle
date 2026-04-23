4ac18391060e205d39059f7528c0859f50f5ca1d
podkorytovkirill24-dot <podkorytovkirill24@gmail.com>
2026-04-23 22:26:06 +0500

    Add files via upload

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,5 @@
+from .admin import router as admin_router
+from .errors import router as errors_router
+from .user import router as user_router
 
+__all__ = ["admin_router", "errors_router", "user_router"]
