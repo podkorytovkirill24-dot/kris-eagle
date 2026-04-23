5362aac05e2c7b02ac1ec8a0256c0c5b3042edee
podkorytovkirill24-dot <podkorytovkirill24@gmail.com>
2026-04-23 22:26:26 +0500

    Add files via upload

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,39 @@
+from .admin import (
+    admin_broadcast_item_keyboard,
+    admin_broadcast_menu_keyboard,
+    admin_main_keyboard,
+    admin_plan_item_keyboard,
+    admin_plans_keyboard,
+    admin_service_actions_keyboard,
+    admin_services_list_keyboard,
+    back_keyboard as admin_back_keyboard,
+    request_decision_keyboard,
+)
+from .user import (
+    back_keyboard as user_back_keyboard,
+    confirm_text_keyboard,
+    crypto_payment_keyboard,
+    main_user_keyboard,
+    payment_keyboard,
+    service_plans_keyboard,
+    services_keyboard,
+)
 
+__all__ = [
+    "admin_broadcast_item_keyboard",
+    "admin_broadcast_menu_keyboard",
+    "admin_main_keyboard",
+    "admin_plan_item_keyboard",
+    "admin_plans_keyboard",
+    "admin_service_actions_keyboard",
+    "admin_services_list_keyboard",
+    "admin_back_keyboard",
+    "confirm_text_keyboard",
+    "crypto_payment_keyboard",
+    "main_user_keyboard",
+    "payment_keyboard",
+    "request_decision_keyboard",
+    "service_plans_keyboard",
+    "services_keyboard",
+    "user_back_keyboard",
+]
